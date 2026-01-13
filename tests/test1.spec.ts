78ed07040970d16764cf8ea4bde6062b4456715a
Deployment-SYS <ttestrsys@gmail.com>
2026-01-13 16:52:52 +0530

    changes

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -3,7 +3,17 @@ import {test,} from '@playwright/test'
 //test.use({baseURL:"https://example.com"})
 test("testcase1: Login",{tag: '@smoke'},async ({page,browser},testinfo)=>{
     await page.goto("/")
-    await page.waitForTimeout(6000)
+    await page.waitForTimeout(2000)
     console.log("Global level" ,testinfo.config)
     console.log("Project level", testinfo.project)
+})
+
+test("TC2",async ({page})=>{
+    await page.goto("https://demo.evershop.io/")
+    await page.waitForTimeout(2000)
+})
+
+test("TC3",async ({page})=>{
+    await page.goto("https://example.com/")
+    await page.waitForTimeout(2000)
 })
